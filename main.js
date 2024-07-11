9f50bac332677ab2f9bdaad81d0b5b82a0c45d65
1zizo1 <ahmedmohmedzizo258@gmail.com>
2024-07-11 17:13:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// var color
+// window.prompt('color please')
+// if (color == 'red') {
+//     console.log('stop')
+// }
+// else if (color == 'green') {
+//     console.log('goooo')
+
+// }
+// else if (color == 'yellow') {
+//     console.log('holld')
+
+// }
+
+// var x = 10;
+// switch (x) {
+//     case x == 10:
+//         console.log('good')
+//         break;
+//     case x < 10:
+//         console.log('high')
+//         break;
+//     case x > 10:
+//         console.log('low')
+//         break;
+
+
+// }
